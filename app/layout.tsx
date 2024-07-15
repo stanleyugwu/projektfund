334cab9f41806b180bb1ccb8b5bf7303cc7a84b3
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-07-15 18:05:32 +0100

    HAndled Responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,13 @@ import { Providers } from '@/context/Providers'
 import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" > 
+    <html lang="en"  > 
       <body className={`${inter.className} `}>
         <Providers>
             {children}
