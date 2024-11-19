fa6fe010b5541e6239a412d79a60324fe3fe6dda
Stanley Ugwu <stanleyugwu2018@gmail.com>
2024-11-19 17:45:34 +0100

    feat: create search component

---
+++ components/ui/search.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+import useDebounce from "@/hooks/useDebounce";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+}
+
+/**
+ * Search inpiut component. works by populating URL
+ * parameters with query so that parent component/page can
+ * intercept that and perform the actual search
+ */
+const Search = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const handleSearch = useDebounce((term: string) => {
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set("query", term);
+      } else {
+        params.delete("query");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 600);
+
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+        defaultValue={searchParams.get("query")?.toString()}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+      />
+    );
+  }
+);
+
+Search.displayName = "Search";
+
+export default Search;
